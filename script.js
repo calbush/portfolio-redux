735e6cd13a204e049c92735a09b28a9ca86b7e54
Caleb Bush <calebmatthewbush@gmail.com>
2023-10-28 16:28:37 -0500

    Build function for modifying opacity of non-hovered buttons

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 const body = document.querySelector('body')
-const foobar = document.createElement('div')
-foobar.innerHTML = 'Holy Moly'
-body.append(foobar)
+
+const hoverFadeBtns = body.querySelectorAll('.hover-fade')
+
+const lowerOpacity = (hoveredButton) => {
+    //find the button we're not hovering, add full-opacity class to it.
+    const targetedButton = hoveredButton.currentTarget
+    targetedButton.setAttribute('id', 'full-opacity')
+
+    const allButtons = document.querySelectorAll('.hover-fade')
+    const allButtonsArray = [...allButtons]
+    const notHovering = allButtonsArray.filter(element => element.id !== 'full-opacity')
+
+    notHovering.forEach(node => {
+        node.classList.add('lower-opacity')
+    })
+}
+
+const resetOpacity = () => {
+    hoverFadeBtns.forEach(btn => {
+        btn.classList.remove('lower-opacity')
+        btn.removeAttribute('id')
+    })
+
+}
+
+hoverFadeBtns.forEach(node => {
+    node.addEventListener('mouseenter', (e) => lowerOpacity(e))
+    node.addEventListener('mouseleave', resetOpacity)
+})
