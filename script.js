d73f7bb5d260c7f566d1c5ab81cc0f67413b6cbf
Caleb Bush <calebmatthewbush@gmail.com>
2023-10-29 23:16:40 -0500

    Write functions for opening and closing modals

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const body = document.querySelector('body')
-
 const hoverFadeBtns = body.querySelectorAll('.hover-fade')
+const modal = body.querySelector('.modal')
+const overlay = body.querySelector('.overlay')
+const projOneBtn = body.querySelector('.proj-one-btn')
+const projTwoBtn = body.querySelector('.proj-two-btn')
+const closeModalBtns = body.querySelectorAll('.btn-close')
 
 const lowerOpacity = (hoveredButton) => {
     //find the button we're not hovering, add full-opacity class to it.
@@ -24,7 +28,32 @@ const resetOpacity = () => {
 
 }
 
+const openOverlay = () => {
+    overlay.classList.remove('hidden')
+}
+
+const openModal = (modal) => {
+    const modalToOpen = body.querySelector(`${modal}`)
+    modalToOpen.classList.remove('hidden')
+}
+
+const closeModalsAndOverlay = () => {
+    const allModals = document.querySelectorAll('.modal')
+    allModals.forEach(modal => modal.classList.add('hidden'))
+    overlay.classList.add('hidden')
+}
+
 hoverFadeBtns.forEach(node => {
     node.addEventListener('mouseenter', (e) => lowerOpacity(e))
     node.addEventListener('mouseleave', resetOpacity)
+    node.addEventListener('click', openOverlay)
 })
+
+closeModalBtns.forEach(btn => btn.addEventListener('click', closeModalsAndOverlay))
+
+projOneBtn.addEventListener('click', () => openModal('.proj-one-modal'))
+projTwoBtn.addEventListener('click', () => openModal('.proj-two-modal'))
+overlay.addEventListener('click', closeModalsAndOverlay)
+
+
+
